c65cd2536d40739e3be450d3139f40ec7cda3ee7
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-15 08:57:45 -0300

    Remove const on internal blocks

--- src/commands/unban.js
+++ src/commands/unban.js
@@ -7,51 +7,51 @@ module.exports.run = async (client, message, args) => {
   let reason = args.slice(1).join(" ");
 
 
-  const member = await client.users.fetch(unbanned)
+  let member = await client.users.fetch(unbanned)
   
-  const ban = await message.guild.fetchBans()
+  let ban = await message.guild.fetchBans()
 
   if (!ban.get(member.id)) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(
         "This user is not banned"
       )
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   if (!message.guild.me.permissions.has('BAN_MEMBERS')) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(
         "I do not have permissions, please contact an administrator"
       )
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   if (!message.member.permissions.has('BAN_MEMBERS')) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(
         "You do not have permission `BAN MEMBERS` contact an administrator"
       )
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
 
-  var user = ban.get(member.id)
+  let user = ban.get(member.id)
   message.guild.members.unban(member);
-  const embed1 = new Discord.MessageEmbed()
+  let embed = new Discord.MessageEmbed()
     .setTitle(`${member.tag} has been successfully unbanned.`)
     .setColor("#2C2F33");
 
-  message.channel.send(embed1)
+  message.channel.send(embed)
 };
 
 module.exports.help = {
