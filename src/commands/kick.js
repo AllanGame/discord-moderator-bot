8fb3461c42b062f0f90fab1845b907c8fd97b871
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-04-15 08:56:19 -0300

    Remove const on internal blocks

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -6,52 +6,52 @@ module.exports.run = async (client, message, args) => {
   if (!kicked) return;
 
   if (message.author === kicked) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(`You cannot sanction yourself`)
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   if (!reason) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(`Enter a reason`)
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   if (!message.member.permissions.has("KICK_MEMBERS")) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(
         "You do not have permission `KICK MEMBERS` contact an administrator"
       )
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   if (!message.guild.me.permissions.has("KICK_MEMBERS")) {
-    const embed = new Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(
         "I do not have `KICK MEMBERS` permission, please contact an administrator"
       )
       .setColor("#2C2F33");
-    message.channel.send({ embed });
+    message.channel.send(embed);
 
     return;
   }
 
   message.guild.member(kicked).kick(reason);
 
-  const embed1 = new Discord.MessageEmbed()
+  let embed = new Discord.MessageEmbed()
     .setDescription(`${kicked.tag} has been successfully kicked.`)
     .setColor("#2C2F33");
 
-  message.channel.send(embed1);
+  message.channel.send(embed);
 };
 
 module.exports.help = {
